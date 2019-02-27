450a6fb811a24fabe263c28546d3bd2d26047e52
MardanovTimur <timurmardanov97@gmail.com>
2019-02-28 02:05:54 +0300

    added search

---
+++ manager3.py
@@ -0,0 +1,87 @@
+import psycopg2
+import numpy
+from porter import StemmerPorter
+from nltk.tokenize import toktok
+
+from nltk.stem.snowball import SnowballStemmer
+
+from nltk.corpus import stopwords
+from typing import Callable
+from pymystem3 import Mystem
+from copy import deepcopy
+
+
+sb_stemmer = SnowballStemmer("russian")
+my_stem_lemmer = Mystem()
+
+
+def create_terms_table(connection, table_name='terms_list'):
+    query = f"""
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    DROP TABLE IF EXISTS {table_name} CASCADE;
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        term_id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+        term_text VARCHAR(64) UNIQUE NOT NULL
+    );
+    """
+    connection.execute(query)
+
+
+def create_article_table(connection, table_name='article_term'):
+    query = f"""
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    DROP TABLE IF EXISTS {table_name} CASCADE;
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        article_id UUID REFERENCES articles (id) ON DELETE CASCADE,
+        term_id UUID REFERENCES terms_list (term_id) ON DELETE CASCADE
+    );
+    """
+    connection.execute(query)
+
+
+def get_porter_words(connection):
+    query = """
+    SELECT term, articles_id FROM words_porter;
+    """
+    connection.execute(query)
+    results = connection.fetchall()
+    insert_into_terms(connection, results)
+
+
+def insert_into_terms(connection, results):
+    query = """
+    INSERT INTO terms_list (term_text) VALUES (%s);
+    """
+    terms = set([item[0] for item in results])
+    for term in terms:
+        connection.execute(query, (term,))
+    query = """
+    SELECT term_text, term_id FROM terms_list;
+    """
+    connection.execute(query)
+    res = dict(connection.fetchall())
+    query = """
+    INSERT INTO article_term (term_id, article_id) VALUES (%s, %s);
+    """
+    for item in results:
+        uid = res[item[0]]
+        connection.execute(query, (uid, item[1]))
+
+
+if __name__ == "__main__":
+    connection = psycopg2.connect(database='postgres',
+                             user='postgres',
+                             password='postgres',
+                             host='0.0.0.0',
+                             port=4321)
+    cursor = connection.cursor()
+    try:
+        create_terms_table(cursor)
+        create_article_table(cursor)
+        get_porter_words(cursor)
+        connection.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        connection.close()
---
+++ search.py
@@ -0,0 +1,92 @@
+import psycopg2
+import numpy
+from porter import StemmerPorter
+from functools import reduce
+from nltk.tokenize import toktok
+
+from collections import OrderedDict
+from nltk.stem.snowball import SnowballStemmer
+
+from nltk.corpus import stopwords
+from typing import Callable
+from pymystem3 import Mystem
+from copy import deepcopy
+
+
+sb_stemmer = SnowballStemmer("russian")
+
+def get_articles(connection, word):
+    query = """
+    SELECT article_id, count(*) as sort FROM article_term
+        INNER JOIN terms_list ON (article_term.term_id = terms_list.term_id)
+    WHERE
+        term_text = %s GROUP BY article_id ORDER BY sort DESC;
+    """
+    connection.execute(query, (word, ))
+    return numpy.array(connection.fetchall())[:, 0]
+
+def lemmatize_search(search: str):
+    toktoks = toktok.ToktokTokenizer()
+    # tokenize words
+    text = list(map(lambda item: item.lower(), toktoks.tokenize(search)))
+    # remove stopwords and punct
+    stop_words = set(stopwords.words('russian'))
+    text = filter(lambda item: not item in stop_words and (item.isalpha() or item.isdigit()), text)
+    return list(map(lambda item: sb_stemmer.stem(item), text))
+
+
+def get_url_by_article_uid(connection, article):
+    query = """
+    SELECT url FROM articles
+        WHERE id = %s
+    """
+    connection.execute(query, (article,))
+    return connection.fetchone()[0]
+
+
+RELEV_COEF = 1.3
+
+support = lambda x: 1 / pow(x + 1, RELEV_COEF)
+
+def merge(a, b):
+    # дает правильную релевантность относительно слияния по AND
+    # сделано по фукции 1/x^1.3 которая подстраивает вероятности схожести
+    res = []
+    func = lambda a: list(map(lambda item: (item[1], support(item[0])), enumerate(a)))
+    a_ = dict(func(a))
+    b_ = dict(func(b))
+    for k in a_.keys():
+        a_[k] = a_[k] + b_[k]
+    return OrderedDict(sorted(a_.items(), key=lambda x: x[1], reverse=True)).keys()
+
+
+def set_articles(results):
+    result_set = set(results[0])
+    for i in range(1, len(results)):
+        result_set_new1 = result_set & set(results[i])
+        result_set_new2 = set(results[i]) & result_set
+        result_set = merge(result_set_new1, result_set_new2)
+    return result_set
+
+
+if __name__ == "__main__":
+    connection = psycopg2.connect(database='postgres',
+                             user='postgres',
+                             password='postgres',
+                             host='0.0.0.0',
+                             port=4321)
+    cursor = connection.cursor()
+    try:
+        search_string = str(input("Input search word: "))
+        lemmas = lemmatize_search(search_string)
+        articles_ids = list(map(lambda lem: get_articles(cursor, lem), lemmas))
+        #  print('standart', reduce(lambda x, y: set(x) & set(y), articles_ids))
+        articles = set_articles(articles_ids)
+
+        print(list(map(lambda item: get_url_by_article_uid(cursor, item), articles)))
+
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        connection.close()
